7d765bbe8454b4dfc16ea8f7e1fca9e4a4e18b97
Dominique Zosso <48818980+dzosso@users.noreply.github.com>
2020-09-11 16:02:45 -0600

    Solver update: removed the summation, replaced with an accumulator

--- PDHG_solve.m
+++ PDHG_solve.m
@@ -7,12 +7,15 @@ function xn = PDHG_solve(A,b,r1,r2,theta)
 
 steps = 15;
 xn = zeros(size(A,2),1);
-yn = zeros(size(A,1),1);
+
+%yn = zeros(size(A,1),1);
+
 xbarn = xn;
 Atb = A'*b;
 AtA = A'*A;
-gradient = zeros(size(A,2),steps+1);
-gradient(:,1) = AtA*xbarn - Atb;
+gradient = AtA*xbarn - Atb;
+%gradient = zeros(size(A,2),steps+1);
+%gradient(:,1) = AtA*xbarn - Atb;
 
 for k = 1:steps
 %     yn = (1/(r1+1))*yn + (r1/(r1+1))*(A*xbarn - b);
@@ -24,16 +27,21 @@ for k = 1:steps
     
     xn_last = xn;
     % calculate gradient step for current k time step
-    gradient_step = zeros(size(A,2),1);
-    for j = 0:k
-        gradient_step =  gradient_step + gradient(:,j+1)*(1/(1+r1)^(k-j+1));
-    end
-    xn = xn - r1*r2*gradient_step;
+    
+%    gradient_step = zeros(size(A,2),1);
+%   for j = 0:k
+%        gradient_step =  gradient_step + gradient(:,j+1)*(1/(1+r1)^(k-j+1));
+%    end
+
+    gradient = (gradient + AtA*xbarn - Atb)/(1+r1);
+
+    xn = xn - r1*r2*gradient;
     
     %projection todo
     
-    xbarn = (1+theta)*xn - theta*xn_last;
-    gradient(:,k+1) = AtA*xbarn - Atb;
+    xbarn = 2*xn - xn_last;
+    
+    %gradient(:,k+1) = AtA*xbarn - Atb;
     
 end
 
