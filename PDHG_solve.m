466b57f3fa6dc43a1e4ce37353079b062e42787e
Dominique Zosso <dominique.zosso@montana.edu>
2020-09-16 16:42:39 -0600

    Cleaned out the PDHG solver for min ||Ax-b|| and added a test script with performance comparison against A\b and conjugate gradients.

--- PDHG_solve.m
+++ PDHG_solve.m
@@ -1,47 +1,28 @@
+function xn = PDHG_solve(A,b,r1,r2,steps)
 % Solves the min |Ax-b| problem using
 % the primal dual hybrid gradient algorithm
 % Input: m by n matrix A, m vector b
-% Ouput: m vector x
+% Ouput: n vector x
 
-function xn = PDHG_solve(A,b,r1,r2,theta)
 
-steps = 15;
 xn = zeros(size(A,2),1);
-
-%yn = zeros(size(A,1),1);
 
 xbarn = xn;
 Atb = A'*b;
 AtA = A'*A;
 gradient = AtA*xbarn - Atb;
-%gradient = zeros(size(A,2),steps+1);
-%gradient(:,1) = AtA*xbarn - Atb;
 
 for k = 1:steps
-%     yn = (1/(r1+1))*yn + (r1/(r1+1))*(A*xbarn - b);
-%     xn_last = xn;
-%     xn = xn - r2*A'*yn;
-%     xbarn = xn +theta*(xn - xn_last);
-%     
-% without dual variable
-    
+
     xn_last = xn;
-    % calculate gradient step for current k time step
-    
-%    gradient_step = zeros(size(A,2),1);
-%   for j = 0:k
-%        gradient_step =  gradient_step + gradient(:,j+1)*(1/(1+r1)^(k-j+1));
-%    end
 
     gradient = (gradient + AtA*xbarn - Atb)/(1+r1);
-
+    
     xn = xn - r1*r2*gradient;
     
     %projection todo
     
     xbarn = 2*xn - xn_last;
-    
-    %gradient(:,k+1) = AtA*xbarn - Atb;
     
 end
 
---
+++ PDHG_solve_test.m
@@ -0,0 +1,35 @@
+m = 10000;
+n = 1000;
+
+A = randn(m,n); % pretty large matrix
+b = randn(m,1); 
+
+% A = [ 1 3 ; 2 -2 ];
+% b = [ 1 ; 2 ];
+
+%% true solution
+
+tic
+x_solution = A \ b;
+toc
+
+%% pdhg
+
+rho2 = normest( A'*A ); % THIS takes more time than solving...
+r1 = 0.5;
+r2 = 1/rho2/r1;
+steps = 70;
+
+toc
+x_pdhg = PDHG_solve( A, b, r1, r2, steps );
+toc
+
+norm(x_solution - x_pdhg) / norm(x_solution)
+
+%% conjugate gradients
+
+tic
+x_cg = pcg(A'*A, A'*b, 1e-12, steps);
+toc
+
+norm(x_solution - x_cg) / norm(x_solution)
