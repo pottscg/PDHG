35d6fc271d2d4cb3a39ba8407b077d4222718c59
Dominique Zosso <48818980+dzosso@users.noreply.github.com>
2020-09-18 15:35:35 -0600

    Update ProjectOntoSimplex.m

--- ProjectOntoSimplex.m
+++ ProjectOntoSimplex.m
@@ -8,7 +8,7 @@ sum_v = sum(v(:));  % compute the current l1-norm
 if sum_v < 1  
   w = v + (1-sum_v)/numel(v);   % if deficient, just uniformly add
 else  
-  u = sort(v,'descend');      % if excessive, "just shrink"
+  u = sort(v(:),'descend');      % if excessive, "just shrink"
   sv = cumsum(u);
   rho = find(u > (sv-1) ./ (1:numel(u))',1,'last');
   theta = max(0, (sv(rho)-1)/rho);
