7ac8a6e2bceba93d600c298752a3e787ecfdfd00
Catherine Potts <pottscg@gmail.com>
2020-10-04 15:28:54 -0600

    Adding file to update B matrix using PDHG algorithm

---
+++ update_B.m
@@ -0,0 +1,29 @@
+function B_new = update_B(B,A,X)
+
+    [n,p] = size(B);
+    B_new = B;
+    [m,n] = size(X);
+    if m >= n
+        rho2 = normest( X'*X ); 
+    else
+        rho2 = normest( X*X' );
+    end
+    r1 = 0.5;
+    r2 = 1/rho2/r1;
+    steps = 10;
+    
+    vk = zeros(n,1);
+    alpha_k_sums = sum(A,2);
+    for k = 1:p
+        if alpha_k_sums(k) > eps
+            % update b_k via algorithm, otherwise no update
+           B_hat = B_new;
+           B_hat(:,k) = [];
+           A_hat = A;
+           A_hat(k,:) = [];
+           vk = (X - X*B_hat*A_hat);
+           vk = (1/alpha_k_sums(k))*sum(vk,2);
+           B_new(:,k) = PDHG_solve(-X,-vk,r1,r2,steps);
+        end
+    end
+end
